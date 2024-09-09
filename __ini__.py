08009a1318efe8da313821c27850f18e903189a6
gomesmr <gomes.mr@gmail.com>
2024-09-09 02:31:31 -0300

    initial commit

--- __ini__.py
+++ __ini__.py
@@ -1,12 +1,12 @@
 # Parametrização das configurações
-source_directory = "g:\\Meu Drive\\MLeva\\drupal_module\\drupal-rest-api-client\\"
-output_file = "mleva_api.txt"
+source_directory = "g:\\foo\\und\\"
+output_file = "output_name.txt"
 
 # Extensões de arquivos que deseja buscar, deixe vazio para pegar todos os arquivos
-file_extensions = ['.md']  # Ou deixe vazio []
+file_extensions = ['.java']  # Ou deixe vazio []
 
 # Extensões de arquivos que você deseja excluir
 exclude_extensions = ['.log', '.tmp']  # Por exemplo, exclua arquivos temporários ou logs
 
 # Diretórios que você deseja excluir (exemplo: excluir o diretório .git)
-exclude_directories = ['.git']
+exclude_directories = ['.git', '.idea']
