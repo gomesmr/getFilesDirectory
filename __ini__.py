7c2619d563811fd10f4019cfeb0099610cd727f6
gomesmr <gomes.mr@gmail.com>
2024-09-09 02:22:33 -0300

    initial commit

---
+++ __ini__.py
@@ -0,0 +1,12 @@
+# Parametrização das configurações
+source_directory = "g:\\Meu Drive\\MLeva\\drupal_module\\drupal-rest-api-client\\"
+output_file = "mleva_api.txt"
+
+# Extensões de arquivos que deseja buscar, deixe vazio para pegar todos os arquivos
+file_extensions = ['.md']  # Ou deixe vazio []
+
+# Extensões de arquivos que você deseja excluir
+exclude_extensions = ['.log', '.tmp']  # Por exemplo, exclua arquivos temporários ou logs
+
+# Diretórios que você deseja excluir (exemplo: excluir o diretório .git)
+exclude_directories = ['.git']
---
+++ main.py
@@ -0,0 +1,40 @@
+import os
+import __ini__  # Importa o arquivo de configurações
+
+
+def gather_files_by_extension(source_directory, output_file, file_extensions, exclude_extensions=None, out_list_directories=None):
+    out_list_directories = out_list_directories or []  # Define como lista vazia se for None
+    exclude_extensions = exclude_extensions or []  # Define como lista vazia se for None
+    with open(output_file, 'w', encoding='utf-8') as outfile:
+        for root, dirs, files in os.walk(source_directory):
+            # Remove os diretórios que estão na lista de exclusão
+            dirs[:] = [d for d in dirs if d not in out_list_directories]
+
+            for file in files:
+                # Se o array de extensões de exclusão estiver vazio, exclui apenas os tipos de arquivo desejados
+                if not any(file.endswith(ext) for ext in exclude_extensions):
+                    # Se o array de extensões estiver vazio, pega todos os arquivos
+                    if not file_extensions or any(file.endswith(ext) for ext in file_extensions):
+                        file_path = os.path.join(root, file)
+
+                        # Escreve o nome e o caminho do arquivo no arquivo de saída
+                        outfile.write(f"Arquivo: {file}\n")
+                        outfile.write(f"Caminho: {file_path}\n")
+                        outfile.write("=" * 40 + "\n")  # Separador para melhor leitura
+
+                        # Lê e escreve o conteúdo do arquivo no arquivo de saída
+                        with open(file_path, 'r', encoding='utf-8') as infile:
+                            outfile.write(infile.read())
+                            outfile.write("\n\n")  # Adiciona uma nova linha entre arquivos
+
+
+# Chama a função com os parâmetros definidos no arquivo __ini__.py
+gather_files_by_extension(
+    __ini__.source_directory,
+    __ini__.output_file,
+    __ini__.file_extensions,
+    __ini__.exclude_extensions,
+    __ini__.exclude_directories
+)
+
+print(f"Conteúdo dos arquivos foi copiado para {__ini__.output_file}")
